ab129425093ce491cf731094f3aabb92136bf7c3
Ethan Miller <ethan.w.miller@gmail.com>
2015-10-27 14:06:40 -0400

    implemented compare method

--- src/main/Anagrams.java
+++ src/main/Anagrams.java
@@ -27,7 +27,7 @@ public class Anagrams {
   }
 
   //subroutine for comparing a string to the existing anagram classes
-  public static void findClass(String s, ArrayList<List<String>> B){
+  private static void findClass(String s, ArrayList<List<String>> B){
 
     for(int i = 0; i < B.size(); i++){
       if(B.get(i) == null){
@@ -46,8 +46,56 @@ public class Anagrams {
   }
 
   //subroutine for comparing two strings to see if they are anagrams of each other
-  public static boolean compare(String s, String t){
-    //TODO: this
+  private static boolean compare(String s, String t){
+    //if s.size != t.size, return false
+    //else make each string into a char array
+    //for each letter in s, ask if t contains that letter
+    //  if not, return false
+    //  else remove that letter from both arrays
+    //compare both arrays. if equal, return true. If unequal (one is not all
+    //null), return false.
+    if(s.length() != t.length()){
+      return false;
+    }
+    else {
+      char[] x = s.toCharArray();
+      char[] y = t.toCharArray();
+      for(int j = x.length; j < x.length; j++){
+        if(contains(x[j], y)) {
+          y[indexOf(x[j], y)] = ' ';
+          x[j] = ' ';
+        }
+        else {
+          return false;
+        }
+      }
+      if(x.equals(y))
+        return false;
+    }
     return true;
+  }
+
+  //check if a char array contains a character
+  private static boolean contains(char c, char[] array){
+    boolean res = false;
+    for(char ch : array){
+      if(c == ch){
+        res = true;
+        break;
+      }
+    }
+    return res;
+  }
+
+  //return index of character in an array
+  private static int indexOf(char c, char[] array){
+    int i = -1;
+    for(int j = 0; j < array.length; j++){
+      if(array[j] == c){
+        i = j;
+        break;
+      }
+    }
+    return i;
   }
 }
