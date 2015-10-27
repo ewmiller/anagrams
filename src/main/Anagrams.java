37c4e2c09e99a0e30655ec06ec81eaf169dbd674
Ethan Miller <ethan.w.miller@gmail.com>
2015-10-27 11:12:58 -0400

    file IO success; also moved dictionaries to new folder within main

--- src/main/Anagrams.java
+++ src/main/Anagrams.java
@@ -1,7 +1,17 @@
 import java.util.*;
+import java.io.*;
 
 public class Anagrams {
   public static void main(String[] args){
-    System.out.println("hey");
+    List<String> A = new ArrayList<String>();
+    try {
+      Scanner fileScanner = new Scanner(new File("./src/main/resources/dict1"));
+      while(fileScanner.hasNextLine()){
+        A.add(fileScanner.nextLine());
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace(System.out);
+    }
+    System.out.println("Success. Line 4 is this: " + A.get(4));
   }
 }
