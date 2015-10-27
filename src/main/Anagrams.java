aa0bbadfa3a7808f255189584ded74d807bc5d36
Ethan Miller <ethan.w.miller@gmail.com>
2015-10-27 12:33:32 -0400

    types are annoying

--- src/main/Anagrams.java
+++ src/main/Anagrams.java
@@ -2,16 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Anagrams {
+
+  //subroutine for comparing a string to the existing anagram classes
+  public static void anagramCompare(String s, List<List<String>> B){
+
+  }
+
+  /* Main method; creates a new array for anagram classes and compares each
+  word in the dictionary to the existing array of classes.
+  Outputs anagram file when complete. */
+
   public static void main(String[] args){
     List<String> A = new ArrayList<String>();
     try {
       Scanner fileScanner = new Scanner(new File("./src/main/resources/dict1"));
+      System.out.println("Reading file: dict1");
       while(fileScanner.hasNextLine()){
         A.add(fileScanner.nextLine());
       }
     } catch (FileNotFoundException e) {
       e.printStackTrace(System.out);
     }
-    System.out.println("Success. Line 4 is this: " + A.get(4));
+
+    //Declares a list of lists
+    List<List<String>> B = new ArrayList<List<String>>();
+    for(String s : A){
+      anagramCompare(s, B);
+    }
   }
 }
